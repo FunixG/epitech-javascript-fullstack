1e5c56c8c73bb49fea5858dbd72e61412f73ebdd
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-02-21 17:12:58 +0100

    done configuring eslint for front

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
-    extends: [
-        "airbnb",
-        "airbnb/hooks",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "airbnb-typescript"
-    ],
-    env: {
-        browser: true,
-        jest: true,
-    },
-    rules: {
-        'react/jsx-filename-extension': [
-            1,
-            {
-                extensions: [
-                '.js',
-                '.jsx',
-                ],
-            },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
         ],
-    },
+      },
+    ],
+  },
 };
