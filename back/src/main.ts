4a94c414d42c3c3c9f5eff59e05a2b2c48fbe304
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-03-12 20:57:57 +0100

    back cors error

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,12 +1,21 @@
 import {NestFactory} from '@nestjs/core';
 import {WsAdapter} from '@nestjs/platform-ws';
 import AppModule from './app.module';
+import * as cors from 'cors';
 
 const APP_PORT: string = process.env.APP_PORT || '8080';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: '*',
+  }));
   await app.listen(APP_PORT);
 }
 bootstrap();
