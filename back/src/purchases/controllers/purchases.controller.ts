ea3458f19bd49d1df6ed1a9055de77d6ed879aee
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-03-13 08:42:41 +0100

    fix lint

--- back/src/purchases/controllers/purchases.controller.ts
+++ back/src/purchases/controllers/purchases.controller.ts
@@ -1,25 +1,18 @@
-import {
-  Controller, Headers, Post, Query, UseGuards,
-} from '@nestjs/common';
+import {Controller, Headers, Post, Query,} from '@nestjs/common';
 import CrudResource from '../../core/resources/crud-resource';
 import Purchase from '../entities/purchase.entity';
 import PurchasesService from '../services/purchases.service';
 import UserService from '../../user/services/user.service';
 import Product from '../../products/entities/product.entity';
 import User from '../../user/entities/user.entity';
-import {ApiBearerAuth} from "@nestjs/swagger";
-import {Roles} from "../../user/entities/roles.decorator";
-import JwtAuthGuard from "../../user/services/jwt.auth.guard";
-import RolesAuthGuard from "../../user/services/roles.auth.guard";
-import {use} from "passport";
-import ProductsService from "../../products/services/products.service";
+import ProductsService from '../../products/services/products.service';
 
 @Controller('purchases')
 export default class PurchasesController extends CrudResource<Purchase, PurchasesService> {
   constructor(
     readonly service: PurchasesService,
     readonly userService: UserService,
-    readonly productService: ProductsService
+    readonly productService: ProductsService,
   ) {
     super(service);
   }
